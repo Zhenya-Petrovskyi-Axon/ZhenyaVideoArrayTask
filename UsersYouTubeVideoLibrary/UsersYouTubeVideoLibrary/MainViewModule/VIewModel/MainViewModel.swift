3506fb33fc43fb726940b977662f0738b415ceb2
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-04-29 12:23:54 +0300

    ViewModel class created

---
+++ UsersYouTubeVideoLibrary/UsersYouTubeVideoLibrary/MainViewModule/VIewModel/MainViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewModel.swift
+//  UsersYouTubeVideoLibrary
+//
+//  Created by Evhen Petrovskyi on 29.04.2021.
+//
+
+import Foundation
+
+protocol MainViewModelProtocol {
+    func setLinks()
+    func saveLink()
+}
+
+class MainViewModel: MainViewModelProtocol {
+    
+    var links: [String] = []
+    
+    func setLinks() {
+        
+    }
+    
+    func saveLink() {
+        
+    }
+    
+    
+}
