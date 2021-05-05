04d9fa4b247dc6c6aa8b0979a54f63d22695b829
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-05-05 12:17:47 +0300

    Start to work with saving objects in service

--- UsersYouTubeVideoLibrary/UsersYouTubeVideoLibrary/Service/CoreData/CoreDataLinkService.swift
+++ UsersYouTubeVideoLibrary/UsersYouTubeVideoLibrary/Service/CoreData/CoreDataLinkService.swift
@@ -5,7 +5,7 @@
 //  Created by Evhen Petrovskyi on 05.05.2021.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 class CoreDataLinkService: LinkDataServiceProtocol {
@@ -16,8 +16,23 @@ class CoreDataLinkService: LinkDataServiceProtocol {
         self.context = context
     }
     
+    // MARK: - Save link to core data
     func saveLink(link: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
         
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "LinkCoreData", in: context)!
+        let url = NSManagedObject(entity: entity, insertInto: context)
+        url.setValue(link, forKeyPath: "urlString")
+        
+        do {
+            try context.save()
+            
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
     }
     
     func getLinks(completion: (([Link]) -> Void)) {
@@ -27,6 +42,4 @@ class CoreDataLinkService: LinkDataServiceProtocol {
     func removeLink(id: String) {
         
     }
-    
-    
 }
