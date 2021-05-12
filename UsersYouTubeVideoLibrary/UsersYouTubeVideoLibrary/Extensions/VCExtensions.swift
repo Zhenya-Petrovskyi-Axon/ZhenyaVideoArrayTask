bfe030feb5a659e3ebc07b950a532eb90ddd3f6f
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-05-12 16:52:02 +0300

    Replaced code

--- UsersYouTubeVideoLibrary/UsersYouTubeVideoLibrary/Extensions/VCExtensions.swift
+++ UsersYouTubeVideoLibrary/UsersYouTubeVideoLibrary/Extensions/VCExtensions.swift
@@ -10,6 +10,7 @@ import AVKit
 
 // MARK: - Use with all UIVIewController Extensions needed
 extension UIViewController {
+    /// Play video from URL with UIKit
     func playVideo(_ url: String) {
         guard let urlToPlay = URL(string: url) else {
             self.showAlert(text: "Url is invalid")
@@ -21,5 +22,14 @@ extension UIViewController {
         self.present(vc, animated: true, completion: {
             vc.player?.play()
         })
+    }
+    /// Show allert to user if error occures
+    func showAlert(text: String) {
+        let alert  = UIAlertController(title: "Important!", message: text, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default) { (_) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 }
